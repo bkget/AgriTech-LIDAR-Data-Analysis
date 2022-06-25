499eb91a065777363903705b117b8fda1404347d
Biruk Getaneh <bkgetmom@gmail.com>
2022-06-26 00:33:55 +0300

    load pipeline

--- scripts/fetch_lidar_data.py
+++ scripts/fetch_lidar_data.py
@@ -38,3 +38,75 @@ class FetchLidarData():
             print(f"Fail to initialize FetchLidarData Class. {e}")
 
 
+    def load_pipeline(self, region: str, bounds: Bounds, epsg:str) -> None:
+        """Loads Pipeline Template to constructe Pdal Pipelines from.
+
+        Parameters
+        ----------
+        file_name : str, optional
+            Path plus file name of the pipeline template if the template is not located in its normal locations,
+            or if another template file is needed to be loaded
+
+        Returns
+        -------
+        None
+        """
+        try:
+            pipeline = {
+                "pipeline": [
+                    {
+                        "bounds": f"{bounds}",
+                        "filename": f"https://s3-us-west-2.amazonaws.com/usgs-lidar-public/{region}/ept.json",
+                        "type": "readers.ept",
+                        "tag": "readdata"
+                    },
+                    {
+                        "limits": "Classification[7:7]",
+                        "type": "filters.range",
+                        "tag": "nonoise"
+                    },
+                    {
+                        "assignment": "Classification[:]=0",
+                        "tag": "wipeclasses",
+                        "type": "filters.assign"
+                    },
+                    {
+                        "out_srs": f"EPSG:{epsg}",
+                        "tag": "reprojectUTM",
+                        "type": "filters.reprojection"
+                    },
+                    {
+                        "tag": "groundify",
+                        "type": "filters.smrf"
+                    },
+                    {
+                        "limits": "Classification[2:2]",
+                        "type": "filters.range",
+                        "tag": "classify"
+                    },
+                    {
+                        "filename": f"{region}.laz",
+                        "inputs": [ "classify" ],
+                        "tag": "writerslas",
+                        "type": "writers.las"
+                    },
+                    {
+                        "filename": f"{region}.tif",
+                        "gdalopts": "tiled=yes,     compress=deflate",
+                        "inputs": [ "writerslas" ],
+                        "nodata": -9999,
+                        "output_type": "idw",
+                        "resolution": 1,
+                        "type": "writers.gdal",
+                        "window_size": 6
+                    }
+                ]
+            }
+            print( pipeline)
+
+            get_logger.info('Successfully Loaded Pdal Pipeline')
+
+        except Exception as e:
+            print('Failed to Load Pdal Pipeline')
+
+
